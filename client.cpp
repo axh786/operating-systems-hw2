5f1ed9152fdb1499e993dbafcb67cded837deccd
axh786 <143470097+axh786@users.noreply.github.com>
2025-03-23 15:06:49 -0500

    Add libararies to client.cpp and server.cpp
    Allow library related function like pthread and sockets to work properly

--- client.cpp
+++ client.cpp
@@ -5,6 +5,34 @@ COSC 3360: Programming Assignment 2
 22 Mar 2025
 */
 
+/*
+struct lines { // defining lines struct from data given by input
+    std::vector<std::pair<char, std::vector<std::pair<int, int> > > >& ranges;
+    std::vector<int> headPos;
+    std::vector<int>& dataPos;
+    int index;
+    char** outputMatrix; // make changes to matrix when done with figuring out character placement
+    char *serverIP
+    char *portNum
+};
+Struct vectors will have to be changed into arrays
+haed pos and data pos will be easily changed to arrays
+ranges will be harder, maybe struct in struct?
+output the matrix using strings and prints no more matrix maybe
+where does lines come into play?
+
+void * decoder(void *void_ptr); is our thread function 
+*/
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <strings.h>
+
 int main(int argc, char *argv[]) {
     
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -4,7 +4,19 @@ Dr. Rincon
 COSC 3360: Programming Assignment 2
 22 Mar 2025
 */
+/*
+have to fork in server.cpp
+*/
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <strings.h>
 
 int main(int argc, char *argv[]) {
     
+    return 0;
 }
