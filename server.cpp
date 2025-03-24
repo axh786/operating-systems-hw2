c888c48271ad73b5a17b58baa782d2bc876b2180
axh786 <143470097+axh786@users.noreply.github.com>
2025-03-24 14:09:35 -0500

    Add comments to server.cpp to outline plan

--- server.cpp
+++ server.cpp
@@ -7,6 +7,14 @@ COSC 3360: Programming Assignment 2
 /*
 have to fork in server.cpp
 we will read the data in order that is sent to us from the client then implement the algorithm to parse and process the data
+
+sendStringData(ptr->ranges, sockfd); // send ranges over to server
+sendMatrixData(ptr->outputMatrix[ptr->index], ptr->cols, sockfd); // send the row that will be edited to the server
+sendIntarrData(ptr->dataPos, ptr->dataSize, sockfd); // sending datapos
+sendIntarrData(ptr->headPos, 2, sockfd); // sending headpos
+readData(ptr->index, ptr->outputMatrix, sockfd); // read data from the server
+
+
 */
 #include <unistd.h>
 #include <iostream>
@@ -17,7 +25,7 @@ we will read the data in order that is sent to us from the client then implement
 #include <netdb.h> 
 #include <strings.h>
 
-/*void matrixEncoder {
+/*void matrixEncoder() { algorithm
 
 }*/
 
