fde9917409f006b7e987e8e4890ee23be1b4dd82
axh786 <143470097+axh786@users.noreply.github.com>
2025-03-26 19:16:15 -0500

    Finish main in server.cpp
    Use template code from server_template.cpp to finish setup server and
    allow it to recieve and process client input

--- server.cpp
+++ server.cpp
@@ -29,7 +29,57 @@ readData(ptr->index, ptr->outputMatrix, sockfd); // read data from the server
 
 }*/
 
-int main(int argc, char *argv[]) {
+void clientInteraction(int newsockfd) {
     
+}
+
+void fireman(int) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2)  {
+        std::cerr << "Port not provided" << std::endl;
+        exit(0);
+    }
+    int sockfd, newsockfd, portno, clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Error opening socket" << std::endl;
+        exit(0);
+    }
+
+    // Populate the sockaddr_in structure
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    // Bind the socket with the sockaddr_in structure
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Error binding" << std::endl;
+        exit(0);
+    }
+
+    // Set the max number of concurrent connections
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    signal(SIGCHLD, fireman);
+
+    while (true) {
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen);
+        if (fork() == 0) {
+            if (newsockfd < 0) {
+                std::cerr << "Error accepting new connections" << std::endl;
+                exit(0);
+            }
+            clientInteraction(newsockfd);
+        }
+    }
+
+    close(sockfd);
     return 0;
 }
